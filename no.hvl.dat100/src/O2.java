67d4e0ffd037c89e7e9133493cce3c5cd2eeddaa
Sootax <3jeunami82lekw@gmail.com>
2024-09-12 01:43:48 +0200

    Add oppgave 2

---
+++ no.hvl.dat100/src/O2.java
@@ -0,0 +1,48 @@
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class O2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            int gradeNumber = input("Poengsum: ");
+            String grade = calculateGrade(gradeNumber);
+            System.out.println(grade);
+        }
+    }
+
+    static int input(String inputText) {
+        boolean validInput = false;
+        int gradeNumber = 0;
+        do {
+            String userInput = showInputDialog(inputText);
+            try {
+                gradeNumber = Integer.parseInt(userInput);
+                if (100 >= gradeNumber && gradeNumber >= 0) {
+                    validInput = true;
+                } else {
+                    System.out.println("Input needs to be between 0 and 100");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Input can only contain numbers.");
+            }
+        } while (!validInput);
+        return gradeNumber;
+    }
+
+    static String calculateGrade(int gradeNumber) {
+        String grade = "";
+        if (gradeNumber > 90) {
+            grade = "A";
+        } else if (gradeNumber > 80) {
+            grade = "B";
+        } else if (gradeNumber > 60) {
+            grade = "C";
+        } else if (gradeNumber > 50) {
+            grade = "D";
+        } else if (gradeNumber > 40) {
+            grade = "E";
+        } else if (gradeNumber <= 39) {
+            grade = "F";
+        }
+        return grade;
+    }
+}
